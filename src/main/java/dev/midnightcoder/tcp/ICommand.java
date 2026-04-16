649f33904ce863742a8d661e5b80e163a6179bca
Glabay <Java_Guru@live.com>
2026-04-15 21:52:10 -0300

    Initial commit

---
+++ src/main/java/dev/midnightcoder/tcp/ICommand.java
@@ -0,0 +1,9 @@
+package dev.midnightcoder.tcp;
+
+/**
+ * @author Glabay | Glabay-Studios
+ * @project MidnightTCP
+ * @social Discord: Glabay
+ * @since 2026-04-15
+*/public interface ICommand {
+}
